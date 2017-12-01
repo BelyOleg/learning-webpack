5bfecfacb95f8f2d3d069cd9e2efdc9b5fed3a9f
Oleg Bely <welcome.bely@gmail.com>
2017-12-01 21:25:34 +0300

    adding uglifyjs plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
  * Created by Bely Oleg on 28.11.2017.
  */
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -9,15 +10,28 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
     module: {
-         rules: [
-           {
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-       }
-     ]
-   }
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            sourceMap: true,
+
+            uglifyOptions: {
+                output: {
+                    comments: false,
+                    beautify: false
+                }
+            }
+        })
+    ]
 };
